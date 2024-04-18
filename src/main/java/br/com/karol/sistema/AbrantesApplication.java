091b36716114e9f1a175c41cd08ab32ac6335722
Junior <jijunior1026@gmail.com>
2024-04-18 20:46:51 -0300

    Criando repositorio com a graça de Deus

---
+++ src/main/java/br/com/karol/sistema/AbrantesApplication.java
@@ -0,0 +1,13 @@
+package br.com.karol.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbrantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/karol/sistema/AbrantesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.karol.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
