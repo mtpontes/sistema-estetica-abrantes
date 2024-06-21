02238c124059e725ef5785140d4fbbaa388f2733
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-21 18:49:42 -0300

    refactor: leve refatoração em `AgendamentoRepository`

--- src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
+++ src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
@@ -1,14 +1,14 @@
 package br.com.karol.sistema.repository;
 
-import br.com.karol.sistema.domain.Agendamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.karol.sistema.domain.Agendamento;
+
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
-    Optional findByDataHora(LocalDateTime dataHora);
+    Optional<Agendamento> findByDataHora(LocalDateTime dataHora);
 }
