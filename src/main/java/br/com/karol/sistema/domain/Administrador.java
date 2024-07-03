0e291627eb7ea6d8511f1eeb8edc5c28b318ffaf
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:48:24 -0300

    refactor: `Administrador`

--- src/main/java/br/com/karol/sistema/domain/Administrador.java
+++ src/main/java/br/com/karol/sistema/domain/Administrador.java
@@ -1,24 +1,35 @@
 package br.com.karol.sistema.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "Administrador")
+@Table(name = "administradores")
 public class Administrador {
     
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    @Column( unique = true, nullable = false)
+    @Column(unique = true, nullable = false)
     private String usuario;
     private String senha;
 
+
+    public Administrador(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha= senha;
+    }
 
     public void update(String usuario) {
         if (usuario != null && !usuario.isBlank()) {
