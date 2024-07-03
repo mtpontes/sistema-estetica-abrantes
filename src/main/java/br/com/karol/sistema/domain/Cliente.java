d710aa76df9883743a95fe1be490bc6ec87b8e50
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:38:12 -0300

    refactor: `Login`
    - Alterar anotações Lombok
    - Corrigir nome da entidade
    - Especificar nome da tabela
    - Alterar ID para Long
    - Adicionar constraint UQ a coluna "telefone"
    - Alterar Endereco para @Embbeded
    
    - Adicionar métodos isBlank, isNull, notBlank e notNull
    - Adicionar novos setters
    - Adicionar método setAll
    - Adicionar construtor personalizado
    - Adicionar método atualizarDados

--- src/main/java/br/com/karol/sistema/domain/Cliente.java
+++ src/main/java/br/com/karol/sistema/domain/Cliente.java
@@ -1,33 +1,116 @@
 package br.com.karol.sistema.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Data
-@Entity(name="Clientes")
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "Cliente")
+@Table(name = "clientes")
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(unique = true)
     private String cpf;
 
     private String nome;
+
+    @Column(unique = true)
     private String telefone;
 
     @Column(unique = true)
     private String email;
 
-    @JoinColumn(name = "paciente_id")
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Endereco> enderecos = new ArrayList<>();
+    @Embedded
+    private Endereco endereco;
+
+
+    public Cliente(String cpf, String nome, String telefone, String email, Endereco endereco) {
+        this.setAll(cpf, nome, telefone, email, endereco);
+    }
+
+    public void atualizarDados(String nome, String telefone, String email, Endereco endereco) {
+        if (!this.isBlank(nome))
+            this.nome = nome;
+
+        if (!this.isBlank(telefone))
+            this.telefone = telefone;
+
+        if (!this.isBlank(email))
+            this.email = email;
+
+        if (!this.isNull(endereco)) {
+            if (this.endereco != null) {
+                this.endereco.atualizarDados(
+                    endereco.getRua(),
+                    endereco.getNumero(), 
+                    endereco.getCidade(), 
+                    endereco.getBairro(), 
+                    endereco.getEstado()
+                );
+                return;
+            }
+            this.endereco = endereco; 
+        }
+    }
+
+    private boolean isNull(Object param) {
+        if (param == null) return true;
+        return false;
+    }
+    private boolean isBlank(String param) {
+        if (param.isBlank()) return true; 
+        return false;
+    }
+
+    private void notNull(Object param, String nomeCampo) {
+        if (this.isNull(param)) 
+            throw new IllegalArgumentException("Não pode ser null: " + nomeCampo);
+    }
+    private void notBlank(String param, String fieldName) {
+        this.notNull(param, fieldName);
+        if (this.isBlank(param))
+            throw new IllegalArgumentException("Não pode ser blank: " + fieldName);
+    }
+
+    public void setCpf(String cpf) {
+        this.notBlank(cpf, "cpf");
+        this.cpf = cpf;
+    }
+    public void setNome(String nome) {
+        this.notBlank(nome, "nome");
+        this.nome = nome;
+    }
+    public void setTelefone(String telefone) {
+        this.notBlank(telefone, "telefone");
+        this.telefone = telefone;
+    }
+    public void setEmail(String email) {
+        this.notBlank(email, "email");
+        this.email = email;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.notNull(endereco, "endereco");
+        this.endereco = endereco;
+    }
+
+    private void setAll(String cpf, String nome, String telefone, String email, Endereco endereco) {
+        this.setCpf(cpf);
+        this.setNome(nome);
+        this.setTelefone(telefone);
+        this.setEmail(email);
+        this.setEndereco(endereco);
+    }
 }
