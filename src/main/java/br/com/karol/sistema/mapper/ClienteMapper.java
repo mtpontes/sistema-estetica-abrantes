164fb58b3454e15756132c9693ae50ea39a940f2
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 14:05:50 -0300

    refactor: Adicionar novos métodos: `ClienteMapper`
    - Adicionar métodos:
        "toCliente",
        "toDadosCompletosDTO",
        "toDadosClienteDTO",
        "toListDadosClienteDTO",
        "toIdNomeEmailClienteDTO",

--- src/main/java/br/com/karol/sistema/mapper/ClienteMapper.java
+++ src/main/java/br/com/karol/sistema/mapper/ClienteMapper.java
@@ -1,62 +1,45 @@
 package br.com.karol.sistema.mapper;
 
 
-import br.com.karol.sistema.domain.Cliente;
-import br.com.karol.sistema.dto.ClienteDTO;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import br.com.karol.sistema.domain.Cliente;
+import br.com.karol.sistema.domain.Endereco;
+import br.com.karol.sistema.dto.cliente.CriarClienteDTO;
+import br.com.karol.sistema.dto.cliente.DadosClienteDTO;
+import br.com.karol.sistema.dto.cliente.DadosCompletosClienteDTO;
+import br.com.karol.sistema.dto.cliente.IdNomeEmailClienteDTO;
+
 @Component
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ClienteMapper {
 
     private ModelMapper mapper;
 
-
-    public ClienteDTO clienteToClienteDTO(Cliente cliente) {
-        return mapper.map(cliente, ClienteDTO.class);
+    public ClienteMapper(ModelMapper mapper) {
+        this.mapper = mapper;
     }
 
-    public Cliente clienteDTOToCliente(ClienteDTO clienteDTO) {
-        return mapper.map(clienteDTO, Cliente.class);
+    
+    public Cliente toCliente(CriarClienteDTO dados) {
+        return new Cliente(dados.getCpf(), dados.getNome(), dados.getTelefone(), dados.getEmail(), mapper.map(dados.getEndereco(), Endereco.class));
     }
 
-    public List<ClienteDTO> clienteListToClienteDTOList(List<Cliente> clienteList) {
-        return clienteList.stream().map(this::clienteToClienteDTO).collect(Collectors.toList());
+    public DadosCompletosClienteDTO toDadosCompletosClienteDTO(Cliente cliente) {
+        return mapper.map(cliente, DadosCompletosClienteDTO.class);
+    }
+    public DadosClienteDTO toDadosClienteDTO(Cliente cliente) {
+        return mapper.map(cliente, DadosClienteDTO.class);
     }
 
-//    public ClienteDTO convertToDTO(Cliente cliente) {
-//        ClienteDTO clienteDTO = new ClienteDTO();
-//        clienteDTO.setId(cliente.getId());
-//        clienteDTO.setNome(cliente.getNome());
-//        clienteDTO.setEmail(cliente.getEmail());
-//        clienteDTO.setTelefone(cliente.getTelefone());
-//        clienteDTO.setEnderecos(cliente.getEnderecos()
-//                .stream()
-//                .map(this::convertToDTO)
-//                .collect(Collectors.toList()));
-//        return clienteDTO;
-//    }
-//
-//    public EnderecoDTO convertToDTO(Endereco endereco) {
-//        EnderecoDTO enderecoDTO = new EnderecoDTO();
-//        enderecoDTO.setId(endereco.getId());
-//        enderecoDTO.setRua(endereco.getRua());
-//        enderecoDTO.setNumero(endereco.getNumero());
-//        enderecoDTO.setCidade(endereco.getCidade());
-//        enderecoDTO.setEstado(endereco.getEstado());
-//        enderecoDTO.setBairro(endereco.getBairro());
-//
-//        return enderecoDTO;
-//    }
+    public List<DadosClienteDTO> toListDadosClienteDTO(List<Cliente> clienteList) {
+        return clienteList.stream().map(c -> toDadosClienteDTO(c)).collect(Collectors.toList());
+    }
 
-
+    public IdNomeEmailClienteDTO toIdNomeEmailClienteDTO(Cliente cliente) {
+        return mapper.map(cliente, IdNomeEmailClienteDTO.class);
+    }
 }
