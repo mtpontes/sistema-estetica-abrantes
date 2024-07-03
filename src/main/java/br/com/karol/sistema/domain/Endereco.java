35046edaa6cb3d6512e613ff40ac253b1b27236b
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:40:10 -0300

    refactor: `Endereco`
    - Alterar anotações Lombok
    - Remover anotações JPA
    - Adicionar anotação @Embbedable
    - Adicionar método "notBlank"
    - Adicionar novos setters
    - Adicionar método "setAll"
    - Adicionar novo construtor
    - Adicionar método "atualizarDados"

--- src/main/java/br/com/karol/sistema/domain/Endereco.java
+++ src/main/java/br/com/karol/sistema/domain/Endereco.java
@@ -1,28 +1,62 @@
 package br.com.karol.sistema.domain;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "endereco")
-@Data
+@Getter
 @NoArgsConstructor
-@AllArgsConstructor
+@Embeddable
 public class Endereco {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String rua;
     private String numero;
     private String cidade;
     private String bairro;
     private String estado;
 
-    @ManyToOne
-    @JoinColumn(name = "cliente_id")
-    private Cliente cliente;
+    public Endereco(String rua, String numero, String cidade, String bairro, String estado) {
+        this.setAll(rua, numero, cidade, bairro, estado);
+    }
 
+    
+    public void atualizarDados(String rua, String numero, String cidade, String bairro, String estado) {
+        this.setAll(rua, numero, cidade, bairro, estado);
+	}
+
+    private void notBlank(String field, String fieldName) {
+        if (field == null)
+            throw new IllegalArgumentException("Não pode ser null: " + fieldName);
+        if (field.isBlank())
+            throw new IllegalArgumentException("Não pode ser blank: " + fieldName);
+    }
+
+    public void setRua(String rua) {
+        this.notBlank(rua, "rua");
+        this.rua = rua;
+    }
+    public void setNumero(String numero) {
+        this.notBlank(numero, "numero");
+        this.numero = numero;
+    }
+    public void setCidade(String cidade) {
+        this.notBlank(cidade, "cidade");
+        this.cidade = cidade;
+    }
+    public void setBairro(String bairro) {
+        this.notBlank(bairro, "bairro");
+        this.bairro = bairro;
+    }
+    public void setEstado(String estado) {
+        this.notBlank(estado, "estado");
+        this.estado = estado;
+    }
+    
+    private void setAll(String rua, String numero, String cidade, String bairro, String estado) {
+        this.setRua(rua);
+        this.setNumero(numero);
+        this.setCidade(cidade);
+        this.setBairro(bairro);
+        this.setEstado(estado);
+    }
 }
