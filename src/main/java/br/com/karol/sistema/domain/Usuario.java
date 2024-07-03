489a5fee2a39bbaa843c7811e78e63df8c53deb8
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:44:09 -0300

    refactor: `Usuario`
    - Alterar anotações Lombok
    - Definir nome da entidade
    - Definir nome da tabela
    - Alterar ID para Long
    - Corrigir método "getPassword"
    
    - Adicionar métodos "notNull" e "notBlank"
    - Adicionar novos setters
    - Adicionar método "atualizarDados"
    - Adicionar novo construtor personalizado

--- src/main/java/br/com/karol/sistema/domain/Usuario.java
+++ src/main/java/br/com/karol/sistema/domain/Usuario.java
@@ -1,44 +1,86 @@
 package br.com.karol.sistema.domain;
 
-import br.com.karol.sistema.enums.UserRole;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
+import br.com.karol.sistema.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@Table(name = "usuario")
-@Entity(name = "usuario")
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
 public class Usuario implements UserDetails {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
+
     @Column(nullable = false)
     private String nome;
+    
     @Column(unique = true, nullable = false)
     private String login;
+    
     @Column(nullable = false)
     private String senha;
 
-    //UserRole(Enum)
+    @Setter
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
+    public Usuario(String nome, String login, String senha) {
+        this.setNome(nome);
+        this.setLogin(login);
+        this.setSenha(senha);
+        this.role = UserRole.USER;
+    }
 
-    public Usuario(String login, String senha, String nome, UserRole role) {
+
+    public void atualizarDados(String nome, String senha) {
+        this.setNome(nome);
+        this.setSenha(senha);
+    }
+
+    public void setNome(String nome) {
+        this.notBlank(nome, "nome");
+        this.senha = nome;
+    }
+
+    public void setLogin(String login) {
+        this.notBlank(login, "login");
         this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.notBlank(senha, "senha");
         this.senha = senha;
-        this.role = role;
-        this.nome = nome;
+    }
+
+    private void notNull(Object field, String fieldName) {
+        if (field == null) 
+            throw new IllegalArgumentException("Não pode ser null: " + fieldName);
+    }
+    private void notBlank(String field, String fieldName) {
+        this.notNull(field, fieldName);
+        if (field.isBlank())
+            throw new IllegalArgumentException("Não pode ser blank: " + fieldName);
     }
 
 
@@ -47,19 +89,19 @@ public class Usuario implements UserDetails {
         if (this.role == UserRole.ADMIN)
             return List.of(
                 new SimpleGrantedAuthority("ROLE_ADMIN"),
-                new SimpleGrantedAuthority("ROLE_USER"));
-        else return List.of(
-            new SimpleGrantedAuthority("ROLE_USER"));
+                new SimpleGrantedAuthority("ROLE_USER")
+            );
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return this.senha;
     }
 
     @Override
     public String getUsername() {
-        return login;
+        return this.login;
     }
 
     @Override
