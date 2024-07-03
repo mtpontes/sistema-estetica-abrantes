55e219afa25627bf387ea0e2096cb73fe94ff916
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:32:18 -0300

    refactor: `Login`
    - Alterar anotações Lombok
    - Corrigir nome da entidade
    - Especificar nome da tabela
    - Adicionar constraint NN a coluna "password"
    - Adicionar métodos `notBlank`, `setLogin`, `setPassword` e `atualizarSenha`
    - Refatorar construtor

--- src/main/java/br/com/karol/sistema/domain/Login.java
+++ src/main/java/br/com/karol/sistema/domain/Login.java
@@ -1,20 +1,56 @@
 package br.com.karol.sistema.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data
-@Entity
+@Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "Login")
+@Table(name = "logins")
 public class Login {
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int id;
-    @Column(unique=true, nullable=false)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
     private String login;
+
+    @Column(nullable = false)
     private String password;
 
+    public Login(String login, String password) {
+        setLogin(login);
+        setPassword(password);
+    }
+
+    public void atualizarSenha(String password) {
+        setPassword(password);
+    }
+
+    private void notBlank(String field, String fieldName) {
+        if (field == null)
+            throw new IllegalArgumentException("Não pode ser null: " + fieldName);
+        if (field.isBlank())
+            throw new IllegalArgumentException("Não pode ser blank: " + fieldName);
+    }
+
+    public void setLogin(String login) {
+        notBlank(login, "login");
+        this.login = login;
+    }
+    public void setPassword(String password) {
+        this.notBlank(password, "password");
+        this.password = password;
+    }
 }
