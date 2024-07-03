c2b6cc9b2ba8b6e8508fcb8f2a4cfbf4c8bbc201
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 14:02:57 -0300

    refactor: `AgendamentoMapper`
    - Adicionar método "toDadosAgendamentoDTO"
    - Adicionar método "toListDadosAgendamentoDTO"

--- src/main/java/br/com/karol/sistema/mapper/AgendamentoMapper.java
+++ src/main/java/br/com/karol/sistema/mapper/AgendamentoMapper.java
@@ -1,32 +1,28 @@
 package br.com.karol.sistema.mapper;
 
-import br.com.karol.sistema.domain.Agendamento;
-import br.com.karol.sistema.dto.agendamento.AgendamentoDTO;
-import lombok.Data;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Data
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import br.com.karol.sistema.domain.Agendamento;
+import br.com.karol.sistema.dto.agendamento.DadosAgendamentoDTO;
+
 @Component
 public class AgendamentoMapper {
 
-    @Autowired
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
 
-    public AgendamentoDTO agendamentoToAgendamentoDTO(Agendamento agendamento) {
-        return mapper.map(agendamento, AgendamentoDTO.class);
+    public AgendamentoMapper(ModelMapper mapper) {
+        this.mapper = mapper;
     }
 
 
-    public Agendamento clienteDTOToCliente(AgendamentoDTO agendamentoDTO) {
-        return mapper.map(agendamentoDTO, Agendamento.class);
+    public DadosAgendamentoDTO toDadosAgentamentoDTO(Agendamento agendamento) {
+        return mapper.map(agendamento, DadosAgendamentoDTO.class);
     }
-
-    public List<AgendamentoDTO> agendamentoListToAgendamentoDTOList(List<Agendamento> agendamentoList) {
-        return agendamentoList.stream().map(this::agendamentoToAgendamentoDTO).collect(Collectors.toList());
+    public List<DadosAgendamentoDTO> toListDadosAgentamentoDTO(List<Agendamento> agendamentoList) {
+        return agendamentoList.stream().map(this::toDadosAgentamentoDTO).collect(Collectors.toList());
     }
 }
