d98a38c2f0ec8f0d42015f2bf9b72217dad4004e
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:58:02 -0300

    build: Mover `LoginResponseDTO` para `br.com.karol.sistema.dto.authentication`

--- src/main/java/br/com/karol/sistema/dto/LoginResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.karol.sistema.dto;
-
-public record LoginResponseDTO(String token) {
-}
---
+++ src/main/java/br/com/karol/sistema/dto/authentication/LoginResponseDTO.java
@@ -0,0 +1,3 @@
+package br.com.karol.sistema.dto.authentication;
+
+public record LoginResponseDTO(String token) {}
