a0a36fb9346931697b14938f7b009295b15f1deb
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 14:12:19 -0300

    refactor: `ClienteException`
    - Adicionar novo construtor padrão se margumentos
    - Adicionar mensagem padrão de erro como constante estática

--- src/main/java/br/com/karol/sistema/exceptions/ClienteException.java
+++ src/main/java/br/com/karol/sistema/exceptions/ClienteException.java
@@ -4,8 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-public class ClienteException extends RuntimeException {
+public class ClienteException extends RuntimeException {;
+
+    private static final String DEFAULT_MESSAGE = "Cliente não encontrado";
+
+
     public ClienteException(String mensagem) {
         super(mensagem);
+    }
+    public ClienteException() {
+        super(DEFAULT_MESSAGE);
     }
 }
