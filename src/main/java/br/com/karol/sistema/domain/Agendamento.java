465d33031c920a5f1a4971314fadee6d9e1afdfb
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-03 13:50:19 -0300

    refactor: `Agendamento`
    - Alterar anotações Lombok
    - Indicar nome da entidade
    - Indicar nome da tabela
    - Alterar ID para Long
    
    - Adicionar método "notNull"
    - Adicionar novos setters
    - Adicionar método "remarcarAgendamento"
    - Refatorar construtor personalizado

--- src/main/java/br/com/karol/sistema/domain/Agendamento.java
+++ src/main/java/br/com/karol/sistema/domain/Agendamento.java
@@ -9,22 +9,25 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "Agendamento")
+@Table(name = "agendamentos")
 public class Agendamento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @ManyToOne(cascade = CascadeType.ALL)
     private Procedimento tipoProcedimento;
+
     private String observacao;
 
     @ManyToOne(cascade = CascadeType.ALL)
@@ -39,14 +42,43 @@ public class Agendamento {
     @ManyToOne
     private Usuario usuario;
 
-
     public Agendamento(Procedimento tipoProcedimento, String observacao, Cliente cliente, LocalDateTime dataHora, Usuario usuario) {
-        this.tipoProcedimento = tipoProcedimento;
-        this.observacao = observacao;
-        this.cliente = cliente;
-        this.dataHora = dataHora;
-        this.usuario = usuario;
-
+        this.setTipoProcedimento(tipoProcedimento);
+        this.setObservacao(observacao);
+        this.setCliente(cliente);
+        this.setDataHora(dataHora);
+        this.setUsuario(usuario);
         this.dataCriacao = LocalDateTime.now();
+    }
+
+
+    public void remarcarAgendamento(String observacao, LocalDateTime dataHora) {
+        this.observacao = observacao; // pode ser blank
+        this.setDataHora(dataHora);
+    }
+
+    private void notNull(Object obj, String nomeCampo) {
+        if (obj == null) throw new IllegalArgumentException("Não pode ser nulo: " + nomeCampo);
+    }
+
+    public void setTipoProcedimento(Procedimento tipoProcedimento) {
+        this.notNull(tipoProcedimento, "tipoProcedimento");
+        this.tipoProcedimento = tipoProcedimento;
+    }
+    public void setObservacao(String observacao) {
+        // Observação: pode ser blank
+        this.observacao = observacao == null ? "" : observacao;
+    }
+    public void setCliente(Cliente cliente) {
+        this.notNull(cliente, "cliente");
+        this.cliente = cliente;
+    }
+    public void setDataHora(LocalDateTime dataHora) {
+        this.notNull(dataHora, "dataHora");
+        this.dataHora = dataHora;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.notNull(usuario, "usuario");
+        this.usuario = usuario;
     }
 }
