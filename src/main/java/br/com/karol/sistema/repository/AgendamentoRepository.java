8229c4a9c8a3ffecc8668ecfb8726f915c6f8af7
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-04 15:15:35 -0300

    refactor: Substituir JPARepository por MongoRepository nos repositórios

--- src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
+++ src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
@@ -3,10 +3,10 @@ package br.com.karol.sistema.repository;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.karol.sistema.domain.Agendamento;
 
-public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+public interface AgendamentoRepository extends MongoRepository<Agendamento, String> {
     Optional<Agendamento> findByDataHora(LocalDateTime dataHora);
 }
