433e8bcd850c3324bdc8760bb573faeb1dab24a5
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-04 08:08:07 -0300

    build: remover entidades e classes relacionadas:
    - Administrador
    - AdministradorController
    - AdministradorMapper
    - AtualizarAdministradorDTO
    - CriarAdminitradorDTO
    - AdministradorRepository
    - DadosAdministradorDTO
    - AdministradorRepository
    - AdministradorService
    
    - Login
    - LoginRepository
    - LoginService
    - LoginController
    
    - Relatorio
    - RelatorioController
    - RelatorioRepository
    - RelatorioService

--- src/main/java/br/com/karol/sistema/repository/AdministradorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.karol.sistema.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.karol.sistema.domain.Administrador;
-
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-}
--- src/main/java/br/com/karol/sistema/repository/RelatorioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.karol.sistema.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.karol.sistema.domain.Relatorio;
-
-public interface RelatorioRepository extends CrudRepository<Relatorio, Long> {
-}
