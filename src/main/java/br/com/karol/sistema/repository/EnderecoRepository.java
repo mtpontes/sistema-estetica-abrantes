27604c549d6e905e73ff2be05584c663050b0869
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-04 15:15:51 -0300

    build: Remover `EnderecoRepository`

--- src/main/java/br/com/karol/sistema/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.karol.sistema.repository;
-
-import br.com.karol.sistema.domain.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
