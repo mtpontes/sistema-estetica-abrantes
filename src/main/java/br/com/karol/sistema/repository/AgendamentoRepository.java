57620cd67b16239701e7738b679543054e3faed6
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-05 11:24:59 -0300

    feat: `AgendamentoRepository`: Adicionar método "findProcedimentoByAgendamentoId"

--- src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
+++ src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
@@ -4,9 +4,13 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import br.com.karol.sistema.domain.Agendamento;
 
 public interface AgendamentoRepository extends MongoRepository<Agendamento, String> {
     Optional<Agendamento> findByDataHora(LocalDateTime dataHora);
+
+    @Query(value = "{ '_id' : ?0 }", fields = "{ 'procedimento._id' : 1 }")
+    Optional<Agendamento> findProcedimentoByAgendamentoId(String agendamentoId);
 }
