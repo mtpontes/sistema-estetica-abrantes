13d25952c16c98604207bc0587b233423022ca1a
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-05 21:26:02 -0300

    feat: `AgendamentoRepository`: Adicionar novos métodos
    - Adicionar método "countByClienteAndDataHoraBetween"
    - Adicionar método "findByAgendamentosBetweenDataHora"

--- src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
+++ src/main/java/br/com/karol/sistema/repository/AgendamentoRepository.java
@@ -1,6 +1,7 @@
 package br.com.karol.sistema.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,4 +14,10 @@ public interface AgendamentoRepository extends MongoRepository<Agendamento, Stri
 
     @Query(value = "{ '_id' : ?0 }", fields = "{ 'procedimento._id' : 1 }")
     Optional<Agendamento> findProcedimentoByAgendamentoId(String agendamentoId);
+
+    @Query("{ 'cliente' : ?0, 'dataHora' : { $gte : ?1, $lte : ?2 } }")
+    Long countByClienteAndDataHoraBetween(String clienteId, LocalDateTime inicio, LocalDateTime fim);
+    
+    @Query("{ 'dataHora' : { $gte : ?0, $lte : ?1 } }")
+    List<Agendamento> findByAgendamentosBetweenDataHora(LocalDateTime inicio, LocalDateTime fim);
 }
