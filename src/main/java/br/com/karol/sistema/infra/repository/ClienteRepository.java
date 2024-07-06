621bc262bdd0a5804f6921792c4fa1a5eb15c9af
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-06 16:25:28 -0300

    feat: `ClienteRepository`: Adicionar métodos "existsByCpf", "existsByTelefone", "existsByEmail"

--- src/main/java/br/com/karol/sistema/infra/repository/ClienteRepository.java
+++ src/main/java/br/com/karol/sistema/infra/repository/ClienteRepository.java
@@ -5,4 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import br.com.karol.sistema.domain.Cliente;
 
 public interface ClienteRepository extends MongoRepository<Cliente, String> {
+
+    boolean existsByCpf(String cpf);
+
+    boolean existsByTelefone(String telefone);
+
+    boolean existsByEmail(String email);
 }
