9c35046fc9812ba8c03f3dc1132015d37025ad9b
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-05 21:25:03 -0300

    refactor: Adicionar @ToString a `Endereco`

--- src/main/java/br/com/karol/sistema/AbrantesApplication.java
+++ src/main/java/br/com/karol/sistema/AbrantesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbrantesApplication {
 
 	public static void main(String[] args) {
+        // TimeZone.setDefault(TimeZone.getTimeZone("UTC-3"));
 		SpringApplication.run(AbrantesApplication.class, args);
 	}
 }
