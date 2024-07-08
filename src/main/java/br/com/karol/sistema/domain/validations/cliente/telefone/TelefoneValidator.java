8f1c967c2d2772541e0e058c4e7ad27b996cfa58
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-08 19:12:42 -0300

    feat: Adicionar interface `TelefoneValidator` e suas implementações:
    PatternTelefoneValidator e UniqueTelefoneValidator

---
+++ src/main/java/br/com/karol/sistema/domain/validations/cliente/telefone/TelefoneValidator.java
@@ -0,0 +1,5 @@
+package br.com.karol.sistema.domain.validations.cliente.telefone;
+
+public interface TelefoneValidator {
+    void validate(String value);
+}
---
+++ src/main/java/br/com/karol/sistema/domain/validations/cliente/telefone/UniqueTelefoneValidator.java
@@ -0,0 +1,22 @@
+package br.com.karol.sistema.domain.validations.cliente.telefone;
+
+import org.springframework.stereotype.Component;
+
+import br.com.karol.sistema.infra.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class UniqueTelefoneValidator implements TelefoneValidator {
+    
+    private static final String CPF_ERROR_MESSAGE = "indisponível";
+
+    private ClienteRepository repository;
+
+
+    @Override
+    public void validate(String value) {
+        if (repository.existsByTelefone(value))
+            throw new IllegalArgumentException(CPF_ERROR_MESSAGE);
+    }
+}
