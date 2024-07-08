611533795f3e896df1689159f98c5728e292b342
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-08 19:32:58 -0300

    refactor: `AtualizarUsuarioDTO`: remover "senha"

--- src/main/java/br/com/karol/sistema/api/dto/usuario/AtualizarUsuarioDTO.java
+++ src/main/java/br/com/karol/sistema/api/dto/usuario/AtualizarUsuarioDTO.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class AtualizarUsuarioDTO {
 
     private String nome;
-    private String senha;
 }
