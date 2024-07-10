1a1a83d11b0be16a9b93c97eb2bfb253803ac6fc
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-10 11:16:05 -0300

    refactor: `FieldValidationException`

--- src/main/java/br/com/karol/sistema/infra/exceptions/FieldValidationException.java
+++ src/main/java/br/com/karol/sistema/infra/exceptions/FieldValidationException.java
@@ -10,13 +10,16 @@ public class FieldValidationException extends RuntimeException {;
     private String errorMessage;
 
     public FieldValidationException(String fieldName) {
-        super(String.format("%s : %s", fieldName, DEFAULT_MESSAGE));
         this.fieldError = fieldName;
         this.errorMessage = DEFAULT_MESSAGE;
     }
     public FieldValidationException(String fieldName, String message) {
-        super(String.format("%s : %s", fieldName, message));
         this.fieldError = fieldName;
         this.errorMessage = message;
+    }
+    
+    @Override
+    public String getMessage() {
+        return String.format("%s : %s", this.fieldError, this.errorMessage);
     }
 }
