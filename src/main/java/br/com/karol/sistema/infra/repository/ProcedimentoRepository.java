43dd8406ad613593349cc5a8f6be7565128a4f3b
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-10 11:14:36 -0300

    feat: `ProcedimentoRepository`: Adicionar método "existsByNome"

--- src/main/java/br/com/karol/sistema/infra/repository/ProcedimentoRepository.java
+++ src/main/java/br/com/karol/sistema/infra/repository/ProcedimentoRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import br.com.karol.sistema.domain.Procedimento;
 
 public interface ProcedimentoRepository extends MongoRepository<Procedimento, String>, ProcedimentoRepositoryCustom {
+
+    boolean existsByNome(String nome);
 }
