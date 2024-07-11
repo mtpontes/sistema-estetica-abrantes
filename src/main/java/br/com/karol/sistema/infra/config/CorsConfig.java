42b53b76cdd12fe5b8146d0baa0a756c2d4664ef
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-11 13:42:48 -0300

    feat: Adicionar `CorsConfig`

---
+++ src/main/java/br/com/karol/sistema/infra/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package br.com.karol.sistema.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
