13508660f17ddae3acaf3dc62df6a4d88b0c3e33
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-15 16:03:33 -0300

    refactor: Ajustar Repositories
    - Ajustar chave
    - Ajuscar consultar MongoDB para SQL
    - Criar consultas personalizadas (substituiem as consultas dos repositórios custom anteriormente utilizadas com MongoDB)

--- src/main/java/br/com/karol/sistema/infra/repository/ProcedimentoRepository.java
+++ src/main/java/br/com/karol/sistema/infra/repository/ProcedimentoRepository.java
@@ -1,10 +1,22 @@
 package br.com.karol.sistema.infra.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.karol.sistema.domain.Procedimento;
 
-public interface ProcedimentoRepository extends MongoRepository<Procedimento, String>, ProcedimentoRepositoryCustom {
+public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long> {
 
     boolean existsByNome(String nome);
+
+    @Query(
+        """
+        SELECT p FROM Procedimento p WHERE
+        (:procedimentoNome IS NULL OR LOWER(p.nome) LIKE LOWER(CONCAT('%', :procedimentoNome, '%')))
+        AND (:minValor IS NULL OR p.valor >= :minValor)
+        AND (:maxValor IS NULL OR p.valor <= :minValor)
+        """)
+    Page<Procedimento> findAllByParams(String procedimentoNome, Double minValor, Double maxValor, Pageable pageable);
 }
--- src/main/java/br/com/karol/sistema/infra/repository/UsuarioRepository.java
+++ src/main/java/br/com/karol/sistema/infra/repository/UsuarioRepository.java
@@ -1,11 +1,11 @@
 package br.com.karol.sistema.infra.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.karol.sistema.domain.Usuario;
 
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     UserDetails findByLoginValue(String login);
     
