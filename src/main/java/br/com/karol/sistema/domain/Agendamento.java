7c82aae378f6ee0293a2b30b3d3ede2d0a3fed25
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-15 16:08:06 -0300

    refactor: Ajustar mapeamento das entidade para JPA

--- src/main/java/br/com/karol/sistema/domain/Agendamento.java
+++ src/main/java/br/com/karol/sistema/domain/Agendamento.java
@@ -3,12 +3,19 @@ package br.com.karol.sistema.domain;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import br.com.karol.sistema.domain.enums.StatusAgendamento;
 import br.com.karol.sistema.domain.validator.agendamento.AgendamentoValidator;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,29 +26,47 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "agendamentos", collation = "pt", language = "portuguese")
+@Entity(name = "Agendamento")
+@Table(name = "agendamentos")
 public class Agendamento {
 
-    @Id
-    private String id;
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String observacao;
+    
+    @Enumerated(EnumType.STRING)
     private StatusAgendamento status;
+    
     @Setter
     private LocalDateTime dataHora;
-    @DBRef
-    private Procedimento procedimento;
-    @DBRef
-    private Cliente cliente;
-    @DBRef
-    private Usuario usuario;
-    private LocalDateTime dataCriacao;
 
-    public Agendamento(Procedimento procedimento, StatusAgendamento status, String observacao, Cliente cliente, LocalDateTime dataHora, Usuario usuario, List<AgendamentoValidator> validators) {
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "procedimento_id")
+    private Procedimento procedimento;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+    
+    private String usuarioLogin;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataModificacao;
+
+    public Agendamento(
+        Procedimento procedimento, 
+        StatusAgendamento status, 
+        String observacao, 
+        Cliente cliente, 
+        LocalDateTime dataHora, 
+        List<AgendamentoValidator> validators
+    ) {
         this.procedimento = this.notNull(procedimento, "procedimento");
 
         List<StatusAgendamento> statusesPermitidos = List.of(StatusAgendamento.PENDENTE, StatusAgendamento.CONFIRMADO);
         if (!statusesPermitidos.contains(status))
-            throw new IllegalArgumentException("Não é possível criar um agendamento com status diferente de: " + statusesPermitidos.toString());
+            throw new IllegalArgumentException(
+                "Não é possível criar um agendamento com status diferente de: " + statusesPermitidos.toString());
         this.status = status;
 
         this.setObservacao(observacao);
@@ -51,25 +76,51 @@ public class Agendamento {
         this.mustBeBeforeNow(dataHora);
         this.dataHora = this.notNull(dataHora, "dataHora");
 
-        this.usuario = this.notNull(usuario, "usuario");
         this.dataCriacao = LocalDateTime.now();
+        this.dataModificacao = dataCriacao;
 
+        if (validators.isEmpty()) 
+            throw new RuntimeException("Deve fornecer um ou mais validadores");
         validators.forEach(validator -> validator.validate(this));
+    }
+    public Agendamento(
+        Procedimento procedimento, 
+        StatusAgendamento status, 
+        String observacao, 
+        Cliente cliente, 
+        LocalDateTime dataHora, 
+        List<AgendamentoValidator> validators, 
+        String usuarioLogin
+    ) {
+        this(procedimento, status, observacao, cliente, dataHora, validators);
+        if (this.notNull(usuarioLogin, "usuarioLogin").isBlank())
+            throw new IllegalArgumentException("Não pode ser blank: usuarioLogin");
+        this.usuarioLogin = usuarioLogin;
     }
 
     public void setObservacao(String observacao) {
         this.observacao = observacao == null ? "" : observacao; // pode ser blank
+
+        this.atualizarDataModificacao();
     }
 
     public void remarcar(LocalDateTime novaDataHora, List<AgendamentoValidator> validators) {
         this.dataHora = novaDataHora;
         this.mustBeBeforeNow(novaDataHora);
+
         validators.forEach(validator -> validator.validate(this));
+        this.atualizarDataModificacao();
     }
 
     public void atualizarStatus(StatusAgendamento novoStatus) {
         this.status.validateTransition(novoStatus);
         this.status = novoStatus;
+
+        this.atualizarDataModificacao();
+    }
+
+    private void atualizarDataModificacao() {
+        this.dataModificacao = LocalDateTime.now(); 
     }
 
     private <T> T notNull(T field, String fieldName) {
