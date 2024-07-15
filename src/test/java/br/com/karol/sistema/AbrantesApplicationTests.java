5e8b4231dbf5bcf5908ee9f4dbc9626bb552f03e
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-15 16:30:55 -0300

    test: Configurar teste de contexto para utilizar banco em memória

--- src/test/java/br/com/karol/sistema/AbrantesApplicationTests.java
+++ src/test/java/br/com/karol/sistema/AbrantesApplicationTests.java
@@ -1,13 +1,14 @@
 package br.com.karol.sistema;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class AbrantesApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
