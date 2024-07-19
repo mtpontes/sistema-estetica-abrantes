25c8da10ec1e425da0eb42ec6d6f3ed659fc2e81
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-19 11:01:19 -0300

    fix: `CriarUsuarioClienteDTO` e `CriarClienteDTO`: Corrigir total de caracteres para os campos "cpf" e "telefone"

--- src/main/java/br/com/karol/sistema/api/dto/cliente/CriarClienteDTO.java
+++ src/main/java/br/com/karol/sistema/api/dto/cliente/CriarClienteDTO.java
@@ -16,14 +16,14 @@ import lombok.Setter;
 public class CriarClienteDTO {
 
     @NotBlank
-    @Size(min = 11, max = 11)
+    @Size(min = 11, max = 14)
     private String cpf;
 
     @NotBlank
     private String nome;
 
     @NotBlank
-    @Size(min = 11, max = 13)
+    @Size(min = 11, max = 15)
     private String telefone;
 
     @NotBlank
