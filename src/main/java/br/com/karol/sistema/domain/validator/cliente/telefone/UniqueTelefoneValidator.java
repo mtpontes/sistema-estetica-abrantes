60839c88395c7daf6dc4478e2802a0879adb386c
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-19 11:06:02 -0300

    fix: `UniqueTelefoneValidator`: Corrigir lançamento de exception

--- src/main/java/br/com/karol/sistema/domain/validator/cliente/telefone/UniqueTelefoneValidator.java
+++ src/main/java/br/com/karol/sistema/domain/validator/cliente/telefone/UniqueTelefoneValidator.java
@@ -2,6 +2,7 @@ package br.com.karol.sistema.domain.validator.cliente.telefone;
 
 import org.springframework.stereotype.Component;
 
+import br.com.karol.sistema.domain.valueobjects.Telefone;
 import br.com.karol.sistema.infra.exceptions.FieldValidationException;
 import br.com.karol.sistema.infra.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UniqueTelefoneValidator implements TelefoneValidator {
     
+    private static final String CLASSE = Telefone.class.getSimpleName();
     private static final String TELEFONE_ERROR_MESSAGE = "indisponível";
 
     private ClienteRepository repository;
@@ -18,6 +20,6 @@ public class UniqueTelefoneValidator implements TelefoneValidator {
     @Override
     public void validate(String value) {
         if (repository.existsByTelefoneValue(value))
-            throw new FieldValidationException(TELEFONE_ERROR_MESSAGE);
+            throw new FieldValidationException(CLASSE, TELEFONE_ERROR_MESSAGE);
     }
 }
