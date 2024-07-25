d90bf18671130ed62a688cc6ed3fc3c29505c052
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-25 19:43:44 -0300

    feat: `AgendamentoConstants`: Adicionar constante `statusesPermitidosParaClients`

--- src/main/java/br/com/karol/sistema/domain/constants/AgendamentoConstants.java
+++ src/main/java/br/com/karol/sistema/domain/constants/AgendamentoConstants.java
@@ -1,6 +1,9 @@
 package br.com.karol.sistema.domain.constants;
 
 import java.time.LocalTime;
+import java.util.List;
+
+import br.com.karol.sistema.domain.enums.StatusAgendamento;
 
 public final class AgendamentoConstants {
     public static final LocalTime HORARIO_ABERTURA = LocalTime.of(8, 0);
@@ -9,4 +12,7 @@ public final class AgendamentoConstants {
     public static final int LIMITE_AGENDAMENTOS_NO_DIA = 2;
     public static final int INTERVALO_ENTRE_AGENDAMENTOS_EM_MINUTOS = 30;
     public static final int ANTECEDENCIA_MINIMA_EM_MINUTOS = 90;
+
+    public static final List<StatusAgendamento> statusesPermitidosParaClients = 
+        List.of(StatusAgendamento.CONFIRMADO, StatusAgendamento.CANCELADO);
 }
