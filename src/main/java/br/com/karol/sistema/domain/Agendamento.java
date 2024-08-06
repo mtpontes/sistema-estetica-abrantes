e811dbacc901325f0e020d8130a9d00ecf237df2
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-06 18:07:04 -0300

    feat: `Agendamento` & `Procedimento`: Adicionar anotação @Builder

--- src/main/java/br/com/karol/sistema/domain/Agendamento.java
+++ src/main/java/br/com/karol/sistema/domain/Agendamento.java
@@ -16,11 +16,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @Getter
 @ToString
 @AllArgsConstructor
--- src/main/java/br/com/karol/sistema/domain/Procedimento.java
+++ src/main/java/br/com/karol/sistema/domain/Procedimento.java
@@ -9,10 +9,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Builder
 @Getter
 @ToString
 @NoArgsConstructor
