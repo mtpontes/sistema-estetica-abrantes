6b4f05179292ba145dcb0ea91f872437e7e9568f
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-16 06:43:25 -0300

    feat: Adicionar EmailDTO

---
+++ src/main/java/br/com/karol/sistema/api/dto/EmailDTO.java
@@ -0,0 +1,17 @@
+package br.com.karol.sistema.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDTO {
+
+    @NotBlank
+    private String email;
+}
