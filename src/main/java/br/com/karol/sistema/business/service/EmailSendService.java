01f56141c848df2b0bd859337f7e2eb1b918cf1c
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-16 06:43:50 -0300

    feat: Adicionar EmailSendService

---
+++ src/main/java/br/com/karol/sistema/business/service/EmailSendService.java
@@ -0,0 +1,50 @@
+package br.com.karol.sistema.business.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import br.com.karol.sistema.api.mapper.EmailMapper;
+import br.com.karol.sistema.domain.valueobjects.Email;
+
+@Service
+public class EmailSendService {
+    
+    private String remetente;
+    private final JavaMailSender javaMailSender;
+    private final EmailMapper emailMapper;
+    private final TokenService tokenService;
+
+    public EmailSendService(
+        @Value("${spring.mail.username}") String remetente,
+        JavaMailSender javaMailSender,
+        EmailMapper emailMapper,
+        TokenService tokenService
+    ) {
+        this.remetente = remetente;
+        this.javaMailSender = javaMailSender;
+        this.emailMapper = emailMapper;
+        this.tokenService = tokenService;
+    }
+
+
+    public void sendEmailVerification(String input) {
+        Email email = emailMapper.toEmail(input); // valida o email
+        String emailToken = tokenService.generateToken(email.getValue());
+
+        SimpleMailMessage emailMessage = new SimpleMailMessage();
+        emailMessage.setFrom(this.remetente);
+        emailMessage.setSubject("Verificação de email");
+        emailMessage.setText(emailToken);
+        emailMessage.setTo(email.getValue());
+        
+        javaMailSender.send(emailMessage);
+    }
+
+    @Async
+    public void sendEmailAsync(SimpleMailMessage mailMessage) {
+        javaMailSender.send(mailMessage);
+    }
+}
